c3ba4f3dc9a7b2ea64facc34363c0f060d9b4bfa
Arnesh1999 <58285955+Arnesh1999@users.noreply.github.com>
2019-11-28 12:45:25 +0530

    First Commit
    Added Unity project to github

---
+++ plane/Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public float speed = 1.0f;
+    public float turnspeed = 100.0f;
+    public float verticalInput;
+    public float horizontalInput;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+       // horizontalInput = Input.GetAxis("Horizontal");
+        //verticalInput = Input.GetAxis("Vertical");
+
+
+
+        transform.Translate(Vector3.forward * Time.deltaTime * speed );
+        if (Input.GetKey("up")) {
+            transform.Rotate(Vector3.left * Time.deltaTime * turnspeed);
+        }
+        else if (Input.GetKey("down"))
+        {
+            transform.Rotate(Vector3.right * Time.deltaTime * turnspeed);
+        }
+        
+
+
+    }
+}
